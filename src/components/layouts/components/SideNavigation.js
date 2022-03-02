ac0f6a80b28b6d42c221e893a1fc56713e84a4e7
Laurence Bautista <rorens05@gmail.com>
2022-03-02 14:09:51 +0800

    Add mobile responsive

--- src/components/layouts/components/SideNavigation.js
+++ src/components/layouts/components/SideNavigation.js
@@ -3,8 +3,13 @@ import { SideNavigationContext } from '../../../context/SideNavigationContext'
 
 export default function SideNavigation({}) {
   const sideNavigationContext = useContext(SideNavigationContext)
-  const {visible} = sideNavigationContext
+  const {visible, toggle} = sideNavigationContext
   return (
-    <div className={`side-navigation ${!visible && "collapsed"}`}>SideNavigation</div>
+    <div className={`side-navigation ${!visible && "collapsed"}`}>
+      <div className='side-toggler cursor-pointer d-none' onClick={toggle}>
+        <i class="fas fa-times"></i>
+      </div>
+      asdfasdf
+    </div>
   )
 }
